c86ebd290d5b70de212560bfadbf5f40754d4413
CalderonCodes <cgcalderondev@gmail.com>
2025-09-09 10:26:23 -0600

    :feat: initial folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Route, Routes } from "react-router";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
   return <div className="bg-background w-full h-screen text-font">
-    Initialize project
+      <Routes>
+        {/* Home element with highest rated anime listing */}
+        <Route path="*" element={<NotFound/>} />
+
+      </Routes>
   </div>;
 }
 
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+
+function NotFound() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+        <h1>404 Page not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
