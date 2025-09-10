69ab233b16665350f878bf73365d4c5734850956
CalderonCodes <cgcalderondev@gmail.com>
2025-09-10 14:59:15 -0600

    :feat: add points dropdown

--- src/features/AddTask/components/PointsDropdown.tsx
+++ src/features/AddTask/components/PointsDropdown.tsx
@@ -1,6 +1,6 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { useState } from "react";
-import { RiUser3Fill } from "react-icons/ri";
+import { RiAddBoxFill } from "react-icons/ri";
 
 type ModalProps = {
   selectedValue: number | undefined;
@@ -20,30 +20,37 @@ function PointsDropdown({ onSelect }: ModalProps) {
   return (
     <Menu>
       <MenuButton className="flex gap-2">
-        {selected === null ? (
+        {selected === undefined ? (
           <span className="flex items-center justify-center gap-2 bg-modal-card py-2 px-4 rounded-sm">
-            <RiUser3Fill />
-            <p>Assignee</p>
+            <RiAddBoxFill />
+            <p>Estimate</p>
           </span>
         ) : (
-          <span className="flex gap-2 w-full items-center text-font font-normal cursor-pointer  px-4 py-2"></span>
+          <span className="flex gap-2 w-full items-center text-font font-normal cursor-pointer  px-4 py-2">
+            <RiAddBoxFill />
+
+            <p>{selected} Points</p>
+          </span>
         )}
       </MenuButton>
       <MenuItems
         anchor="bottom start"
-        className="bg-background-modal border-1 border-accent-hover rounded-lg text-font mt-2 w-[16rem] flex flex-col"
+        className="bg-background-modal border-1 border-accent-hover rounded-lg text-font mt-2 flex flex-col"
       >
         <MenuItem>
           <span className="text-font-secondary font-semibold text-lg  px-4 py-2">
-            Assign To...
+            Estimate
           </span>
         </MenuItem>
         {estimatePoints.map((number) => (
           <MenuItem key={number}>
             <span
-              className="flex gap-2 w-full items-center text-font font-normal hover:bg-modal-card cursor-pointer  px-4 py-2"
+              className="flex gap-2 items-center text-font font-normal hover:bg-modal-card cursor-pointer  px-4 py-2"
               onClick={() => handleSelect(number)}
-            ></span>
+            >
+              <RiAddBoxFill />
+              <p>{number} Points</p>
+            </span>
           </MenuItem>
         ))}
       </MenuItems>
